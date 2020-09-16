171c819a4ddcd6c86df267b973499dc622ecd4db
AshutoshPatole <apatole479@gmail.com>
2020-09-16 13:40:46 +0530

    Day 32.1

---
+++ CPP programs/easy programs/Ichihime_and _Triangle.cpp
@@ -0,0 +1,17 @@
+/// problem link => https://codeforces.com/problemset/problem/1337/A
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+
+    int tests;
+    cin >> tests;
+    for (int i = 0; i < tests; i++)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << b << " " << c << " " << c << endl;
+    }
+    return 0;
+}
